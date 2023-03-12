885ec6cbc8c433d91e636f502c95170d6d75757d
darwin <dazhurgen@gmail.com>
2023-03-11 22:10:14 -0400

    create controllers, modules and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ReaderModule } from './reader/reader.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ReaderModule],
   controllers: [AppController],
   providers: [AppService],
 })
